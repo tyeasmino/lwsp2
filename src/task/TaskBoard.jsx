b871664f110a05e02d1b68f6c4b723bcbc2a3b09
tyeasmino <tyeasmino@gmail.com>
2025-05-15 19:38:28 +0600

    Save Edit Done

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import AddTaskModal from './AddTaskModal';
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
@@ -7,20 +8,54 @@ import TaskList from "./TaskList";
 export default function TaskBoard() {
     const defaultTask = {
         'id': crypto.randomUUID(),
-        'title': "Learn React",
+        'title': "Learn React ",
         'description': "I want to learn React such than I can treat it like my slave and make it do whateer I wan to do",
-        "tags": ['we', 'react', 'js'],
+        "tags": ["web", "react", "js"],
         'priority': " High",
-        'isFavorite': true,
+        'isFavorite': false,
     }
-    const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState([defaultTask]);
+    const [showAddModal, setShowAddModal] = useState(false);
+    const [taskToUpdate, setTaskToUpdate] = useState(null);
 
+    function handleAddEditTask(newTask, isAdd) {
+        if (isAdd) {
+            setTasks([
+                ...tasks,
+                newTask
+            ])
+        } else {
+            setTasks(tasks.map((task) => {
+                if (task.id === newTask.id) {
+                    return newTask;
+                }
+                return task;
+            }))
+        }
+
+        setShowAddModal(false)
+    }
+
+    function handleEditTask(task) {
+        setTaskToUpdate(task)
+        setShowAddModal(true)
+    }
+
+    function handleCloseClick() {
+        setShowAddModal(false);
+        setTaskToUpdate(null);
+    }
 
 
     return (
         <div>
             <section className="mb-20" id="tasks">
-
+                {showAddModal && (
+                    <AddTaskModal
+                        onSave={handleAddEditTask}
+                        taskToUpdate={taskToUpdate}
+                        onCloseClick={handleCloseClick}
+                    />)}
                 <div className="container">
 
                     <div className="p-2 flex justify-end">
@@ -28,8 +63,8 @@ export default function TaskBoard() {
                     </div>
 
                     <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-                        <TaskActions />
-                        <TaskList tasks={tasks} />
+                        <TaskActions onAddClick={() => setShowAddModal(true)} />
+                        <TaskList tasks={tasks} onEdit={handleEditTask} />
                     </div>
                 </div>
             </section>
